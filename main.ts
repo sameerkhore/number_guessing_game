069037adeab473ff394c14db52ca3f09c0305f6e
sameerkhore <159932381+sameerkhore@users.noreply.github.com>
2024-04-21 14:25:41 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const randomNumber =27;
+const answer = await inquirer.prompt([
+{  name: "userguessedNumber",
+  type: "number",
+  message: "please Guess a Number between 1-30."
+},
+]);
+
+if (answer.userguessedNumber === randomNumber) {
+    console.log("congragulation! you gussed right number.");
+}
+else{
+    console.log("you guessed wrong number.");
+}
